5d4820030838019cbad5639918fc4dc74a1af196
bathulapraka <bathulaprakash406@gmail.com>
2024-02-28 22:33:08 +0530

    summary

--- Components/Layout/Header.js
+++ Components/Layout/Header.js
@@ -2,17 +2,20 @@ import React from "react";
 import meals from "../Images/Meals1.avif";
 import "../Layout/Header.css";
 import Headercart from "./HeaderCart";
+import Paragraph from "../Summary/Summary";
 const Header = (props) => {
   return (
     <>
       <header className="Header-things">
         <h1>Restaurent</h1>
-        <Headercart/>
+        <Headercart />
       </header>
-      
+
       <div>
         <img src={meals} alt="Food-items" className="Meals1"></img>
+        <Paragraph />
       </div>
+     
     </>
   );
 };
