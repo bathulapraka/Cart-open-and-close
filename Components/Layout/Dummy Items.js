baaf3f9745973e0b43e1b0ff1f1b1b9063cc2412
bathulapraka <bathulaprakash406@gmail.com>
2024-02-29 11:46:56 +0530

    Add a form

--- Components/Layout/Dummy Items.js
+++ Components/Layout/Dummy Items.js
@@ -1,5 +1,7 @@
 import React from "react";
+import "../Layout/Header.css";
 import "../Layout/Header.css"
+import User from "../User Input/User Input";
 const Dummy = (props) => {
   const Arr = [
     {
@@ -24,16 +26,16 @@ const Dummy = (props) => {
     },
   ];
   return (
-    <div className="All" >
-      <div  className="All-dummy-items">
+    <div className="All">
+      <div className="All-dummy-items">
         {Arr.map((item, index) => (
           <li key={index} className="data">
             <h2>{item.name}</h2>
             <p>{item.item}</p>
             <h2 className="Amount">{item.value}</h2>
+            <User />
           </li>
         ))}
-        
       </div>
     </div>
   );
